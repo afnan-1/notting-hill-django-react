ebcdb5c7918594a265d14d98466a328614f20a8b
afnan-1 <roman.nadeem@gmail.com>
2021-07-23 09:34:41 -0700

    updated build

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8000';
+const baseURL = 'http://ec2-13-233-173-82.ap-south-1.compute.amazonaws.com:8000';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
