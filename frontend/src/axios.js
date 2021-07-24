29eb156df4a6bbbade717f5aaa80e1884dca1de8
afnan-1 <roman.nadeem@gmail.com>
2021-07-24 04:38:41 -0700

    deleted old build

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseURL = 'http://ec2-13-233-173-82.ap-south-1.compute.amazonaws.com:8000';
+const baseURL = 'http://ec2-13-127-240-132.ap-south-1.compute.amazonaws.com:8000';
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
-    timeout: 5000,
+    timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
     },
