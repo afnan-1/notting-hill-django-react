af903850bff65ac8071a2b5e99ed5aaf7fdd63c4
afnan-1 <roman.nadeem@gmail.com>
2021-08-17 00:40:44 +0500

    env removed

--- server/nottinghill/api/resources/models.py
+++ server/nottinghill/api/resources/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import FileExtensionValidator
 
+
 # Create your models here.
 class GuidesResources(models.Model):
     thumbnail_title = models.CharField(max_length=200)
